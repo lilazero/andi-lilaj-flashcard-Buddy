3484b28936392500ee19983feed1344df26cbe9e
lilazero <andililajal@gmail.com>
2025-10-18 10:00:10 +0200

    Added two test cards

--- src/components/FlashcardBuddy.tsx
+++ src/components/FlashcardBuddy.tsx
@@ -4,6 +4,31 @@
 const STORAGE_KEY = "flashcard_buddy_cards";
 export default function FlashcardBuddy() {
 
+const [cards, setCards] = useState<Card[]>([]); 
+
+
+// 2 test cards
+// TODO! delete these later
+useEffect(() => {
+  const testCard: Card = {
+    id: 1,
+    front: "Was ist die Hauptstadt der USA?",
+    back: "Langley Falls, Virginia",
+    showAnswer: false,
+  };
+
+
+  const testCard2: Card = {
+    id: 2,
+    front: "Was ist die Hauptstadt von Frankreich?",
+    back: "Frankfurt",
+    showAnswer: false,
+  };
+
+  setCards([testCard, testCard2]);
+}, []);
+
+
 
 
   return (
